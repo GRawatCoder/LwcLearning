91e94cfc33f81760f456a0032558be2fa8291e75
GRawatCoder <gaurav.rawat194@gmail.com>
2023-07-18 17:41:31 +0530

    added button click and play with dom

--- force-app/main/default/lwc/templateQuerySelector/templateQuerySelector.js
+++ force-app/main/default/lwc/templateQuerySelector/templateQuerySelector.js
@@ -2,5 +2,17 @@ import { LightningElement } from 'lwc';
 
 export default class TemplateQuerySelector extends LightningElement {
     brands = ["mercedes", "bmw", "maruti", "tata"];
-    
+    fetchDetailHandler(){
+        const elem = this.template.querySelector("h1");
+        elem.style.border = '2px solid red';
+
+        const elements = this.template.querySelectorAll('.name');
+        Array.from(elements).forEach(item=>{
+            item.setAttribute("title", item.innerText);
+        });
+
+        ///  lwc:dom="manual" demo
+        const childElem = this.template.querySelector('.child')
+        childElem.innerHTML = '<p>Hey i am a child element</p>'
+    }
 }

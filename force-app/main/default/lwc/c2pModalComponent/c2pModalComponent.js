b09d2af6dd45a9bf5990d08294b7515dc8a5d776
GRawatCoder <gaurav.rawat194@gmail.com>
2023-07-28 10:31:53 +0530

    event bubbling child to parent

--- force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
+++ force-app/main/default/lwc/c2pModalComponent/c2pModalComponent.js
@@ -3,8 +3,12 @@ import { LightningElement } from 'lwc';
 export default class C2pModalComponent extends LightningElement {
     closeHandler(){
         const event = new CustomEvent('close', {
+            bubbles:true,
             detail:'Modal closed successfully!'
         });
         this.dispatchEvent(event);
+    }
+    footerHandler(){
+        console.log('child event bubble');
     }
 }

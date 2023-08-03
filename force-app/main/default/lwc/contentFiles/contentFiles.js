604e4334108fc5b1a1d8fb3c23b1144c2e3d3744
GRawatCoder <gaurav.rawat194@gmail.com>
2023-08-03 15:30:49 +0530

    fetch pdf from content asset

---
+++ force-app/main/default/lwc/contentFiles/contentFiles.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import CONTENTFile from '@salesforce/contentAssetUrl/UdemPD1pdf';
+
+export default class ContentFiles extends LightningElement {
+    content = CONTENTFile;
+}

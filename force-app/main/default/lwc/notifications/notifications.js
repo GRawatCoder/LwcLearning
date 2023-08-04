014d37431c78a6d569f9a7ac8e11e6b5d094bcf9
GRawatCoder <gaurav.rawat194@gmail.com>
2023-08-04 15:21:05 +0530

    add toast notification component

---
+++ force-app/main/default/lwc/notifications/notifications.js
@@ -0,0 +1,33 @@
+import { LightningElement } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+
+export default class Notifications extends LightningElement {
+    _title='write your title here...'
+    message = 'your message here';
+    variant = 'error';
+    variantOptions=[
+        { label: 'error', value: 'error' },
+        { label: 'warning', value: 'warning' },
+        { label: 'success', value: 'success' },
+        { label: 'info', value: 'info' }
+    ];
+
+    titleChange(event){
+        this._title = event.target.value;
+    }
+    messageChange(event){
+        this.message = event.target.value;
+    }
+    variantChange(event) {
+        this.variant = event.target.value;
+    }
+    showNotification(){
+        const evt = new ShowToastEvent({
+            title:this._title,
+            message:this.message,
+            variant:this.variant,
+            mode:'sticky'
+        });
+        this.dispatchEvent(evt);
+    }
+}
